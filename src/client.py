2a07c86dfbb98687f628610ce2008f316c51512c
juan2511 <juann.vanecek@gmail.com>
2014-06-14 11:45:49 -0300

    Medicion tiempos. Estable

--- src/client.py
+++ src/client.py
@@ -1,36 +1,67 @@
 #!/bin/usr
 # Script que abre un socket y lo conecta al puerto 6677 de localhost.
 
+from constants import *
+import sys
 import time
 from ptc import Socket, SHUT_WR
 
-received = str()
+times = []
 
-sock2 = Socket() 
-sock2.connect(('127.0.0.1', 6677))
+def start_sending(delay, perdida, sizes_to_send):
+	print "Vas a mandar paquetes de tamano: ", sizes_to_send
+	received = str()
 
-while True:
-	
-	size_to_send = raw_input('Bytes a enviar: ')
-	
-	# aviso cuanto voy a mandar para que el server se prepare
-	if type(size_to_send) != int or size_to_send < 1: 
-		sock2.send( "0" )
-		break
+	with Socket(delay, perdida) as sock2:
+		sock2.connect(  CONNECTION_DIR  )
 
-	size_to_send = int( size_to_send )
-	to_send = "a"*size_to_send
-		
-	received = sock2.recv(3)
+		for size_to_send in sizes_to_send:
+			# si no es un size valido salgo
+			if not size_to_send.isdigit() or int(size_to_send) < 1: 
+				break
 
-	# si el server nos contesto, significa que ya le podemos empezar a enviar.
-	startTime = time.time()
-	sock2.send( to_send )
-	timeToServer = float( sock2.recv(12) )
-	totalTime = time.time() - startTime
-	
-	print 'sock2 received: (%.10f, %.10f)' % (timeToServer, totalTime)
+			timeToServerPromedio = 0
+			totalTimePromedio = 0
+			for i in range(REPEAT_SEND):
+				# aviso cuanto voy a mandar para que el server se prepare y espero a q me confirme si esta listo
+				sock2.send( size_to_send )
+				received = sock2.recv(LEN_SERVER_MSG_OK)
 
-# Cerramos el stream de escritura pero podemos seguir recibiendo datos.
-sock2.shutdown(SHUT_WR)
-sock2.close()
+				# empiezo a mandar
+				to_send = "a"*int(size_to_send)
+
+				# si el server nos contesto, significa que ya le podemos empezar a enviar.
+				startTime = time.time()
+				sock2.send( to_send )
+				timeToServer = float( sock2.recv(12) )
+				totalTime = time.time() - startTime
+
+				timeToServerPromedio += timeToServer / REPEAT_SEND
+				totalTimePromedio += totalTime / REPEAT_SEND
+
+			print 'sock2 received: (%.10f, %.10f)' % (timeToServerPromedio, totalTimePromedio)
+
+			times.append({
+				'size' : size_to_send, 
+				'timeServer' : timeToServerPromedio,
+				'totalTime' : totalTimePromedio
+			})
+
+		# aviso al server que termino todo
+		sock2.send( NULL_SIZE )
+
+		# Cerramos el stream de escritura pero podemos seguir recibiendo datos.
+		sock2.shutdown(SHUT_WR)
+		sock2.close()
+
+		print times
+
+if __name__ == "__main__":
+	delay = 0 if not len(sys.argv) > 1 else int(sys.argv[1])
+	perdida = 0 if not len(sys.argv) > 2 else float(sys.argv[2])
+
+	sizes_to_send = []
+	for i in range(3, len(sys.argv)):
+		sizes_to_send.append( sys.argv[i] )
+
+	start_sending(delay, perdida, sizes_to_send)
--- src/server.py
+++ src/server.py
@@ -1,30 +1,39 @@
 #!/bin/usr
 # Script que liga un socket al puerto 6677 de localhost.
 
+from constants import *
+import sys
 import time
 from ptc import Socket
 
-sock1 = Socket() 
-sock1.bind(('127.0.0.1', 6677))
-sock1.listen()
-sock1.accept()
+def start_server(delay=0, perdida=0):
+	with Socket(delay, perdida) as sock1:
+		sock1.bind( CONNECTION_DIR )
+		sock1.listen()
+		sock1.accept()
 
-while True:
-	# me dicen cuanto voy a recibir
-	size_to_recv = sock1.recv(10)
+		while True:
+			# me dicen cuanto voy a recibir
+			size_to_recv = sock1.recv(4)
 
-	if size_to_recv == "0":
-		break
+			if size_to_recv == NULL_SIZE:
+				break
 
-	# contesto que puedo empezar a recibir
-	sock1.send("ok!")
+			# contesto que puedo empezar a recibir
+			sock1.send(SERVER_MSG_OK)
 
-	startTime = time.time()
-	str_recv = sock1.recv( int(size_to_recv) )
-	totalTime = time.time() - startTime
+			startTime = time.time()
+			str_recv = sock1.recv( int(size_to_recv) )
+			totalTime = time.time() - startTime
 
-	# le mando cuanto tardo en recibir
-	sock1.send( "%.10f" % totalTime )	
-	print 'size_to_recv = %s\nstr_recv = %s\nserverTime = %.10f' % (size_to_recv, str_recv, totalTime)
+			# le mando cuanto tardo en recibir
+			sock1.send( "%.10f" % totalTime )	
+			print 'size_to_recv = %s\nstr_recv = %s\nserverTime = %.10f' % (size_to_recv, str_recv, totalTime)
 
-sock1.close()
+		sock1.close()
+
+if __name__ == "__main__":
+	delay = 0 if not len(sys.argv) > 1 else int(sys.argv[1])
+	perdida = 0 if not len(sys.argv) > 2 else float(sys.argv[2])
+
+	start_server(delay, perdida)
